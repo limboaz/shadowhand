a55ec4b2b43d92555588df72d515b41d0bc78857
limboaz <limboaz@outlook.com>
2021-05-20 01:23:34 -0400

    finalize

--- stockdataproc.py
+++ stockdataproc.py
@@ -1,19 +1,19 @@
 import yfinance as yf
 
-data = yf.download('GME','2020-12-01','2021-05-01')
+data = yf.download('MVIS','2020-12-31','2021-05-01')
 
 print(data)
 
-data.to_csv('gme_stock_prices.csv')
+data.to_csv('mvis_stock_prices.csv')
 
-data = yf.download('AMC','2020-12-01','2021-05-01')
+# data = yf.download('AMC','2020-12-01','2021-05-01')
 
-print(data)
+# print(data)
 
-data.to_csv('amc_stock_prices.csv')
+# data.to_csv('amc_stock_prices.csv')
 
-data = yf.download('BB','2020-12-01','2021-05-01')
+# data = yf.download('BB','2020-12-01','2021-05-01')
 
-print(data)
+# print(data)
 
-data.to_csv('bb_stock_prices.csv')
+# data.to_csv('bb_stock_prices.csv')
